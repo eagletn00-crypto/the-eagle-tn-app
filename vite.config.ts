09ab0410c2988e6f7e2cdba25ff38fad7a407c2c
eagletn00-crypto <eagletn00@gmail.com>
2026-05-20 03:32:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // إضافة هذا السطر هو مفتاح الحل للمسارات في Vercel
+  base: './', 
   server: {
     host: true,
     port: 5173
